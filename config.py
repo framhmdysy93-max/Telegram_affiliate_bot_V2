f61ed994381590ae2bd3d70e921e52dc94bd271c
framhmdysy93-max <framhmdysy93@gmail.com>
2025-10-31 13:42:43 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# إعدادات المحفظة والدفع
+wallet_data = {
+    "description": "دفع مقابل الخدمة",
+    "wallet_address": "123456789ABCDEF"  # ← اكتبي عنوان محفظتك هنا
+}
+
+# روابط الدفع حسب المبلغ
+amount_data = {
+    "5$": "https://example.com/pay/5",
+    "10$": "https://example.com/pay/10",
+    "20$": "https://example.com/pay/20"
+}
